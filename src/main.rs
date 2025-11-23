2726ff76e789cd91a8bc374c7de77d04da288af1
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-23 19:43:23 +0530

    define registers

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 
 use std::fs;
 
-const MEMORY_SIZE :usize = 65535;
+// virtual machine config
+const MEMORY_SIZE :usize = 65536;
 const STACK_SIZE :usize = 4096;
 const REGISTER_NUM :usize = 8;
+
+const REG_PC :usize = 0;    // program counter
+const REG_SP :usize = 1;    //stack pointer
+const REG_A :usize = 2;     // 2 - 5 genral purpose
+const REG_B :usize = 4;
+const REG_C :usize = 4;
+const REG_D :usize = 5;
+const REG_BP :usize = 6; // base pointer
+const REG_FLAG :usize = 7;
 
 fn main() {
     println!("Hello, world!");
