0226d74cd925525d69f17f7d6b36d23814215731
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-23 21:29:51 +0530

    add flag bits

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,13 @@ const REG_B :usize = 4;
 const REG_C :usize = 4;
 const REG_D :usize = 5;
 const REG_BP :usize = 6; // base pointer
-const REG_FLAG :usize = 7;
+const REG_FLAGS :usize = 7; // flage register
+
+//flag bits
+const FLAG_ZERO :u16 = 1 << 0;
+const FLAG_CARRY :u16 = 1 << 1;
+const FLAG_SIGN :u16 = 1 << 2;
+const FLAG_OVERFLOW :u16 = 1 << 3;
 
 fn main() {
     println!("Hello, world!");
